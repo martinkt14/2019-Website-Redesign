1adc711612f3780c91f7680aa090051c48f23971
Martin <teufel.m14@gmail.com>
2019-09-30 19:49:04 -0400

    Temporary coming soon page

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ app.use(express.static(publicDirectoryPath));
 // });
 
 app.listen(PORT, () => {
-  // console.log(path.join(__dirname, "/public"));
-  console.log("Server is up on port " + PORT);
+    // console.log(path.join(__dirname, "/public"));
+    console.log("Server is up on port " + PORT);
 });
